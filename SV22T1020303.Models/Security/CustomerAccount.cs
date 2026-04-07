03d10c785f23e3733f5e43979eaede2498ecbca0
SV22T1020303 <22T1020303@husc.edu.vn>
2026-04-07 23:34:44 +0700

    Add files via upload

---
+++ SV22T1020303.Models/Security/CustomerAccount.cs
@@ -0,0 +1,10 @@
+﻿namespace LiteCommerce.Models.Security
+{
+    public class CustomerAccount
+    {
+        public int CustomerID { get; set; }
+        public string Email { get; set; } = "";
+        public string PasswordHash { get; set; } = "";
+        public bool IsActive { get; set; }
+    }
+}
